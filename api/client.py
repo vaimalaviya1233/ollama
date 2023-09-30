b66ed7d81a801e3f77e99dd9cf83a83cda92e584
Michael Yang <mxyng@pm.me>
2023-09-29 20:31:38 -0700

    pythonic python client

--- api/client.py
+++ api/client.py
@@ -1,225 +1,143 @@
 import os
 import json
 import requests
+from urllib.parse import urljoin
 
-BASE_URL = os.environ.get('OLLAMA_HOST', 'http://localhost:11434')
+base = os.getenv('OLLAMA_HOST', 'http://localhost:11434')
 
-# Generate a response for a given prompt with a provided model. This is a streaming endpoint, so will be a series of responses.
-# The final response object will include statistics and additional data from the request. Use the callback function to override
-# the default handler.
-def generate(model_name, prompt, system=None, template=None, context=None, options=None, callback=None):
-    try:
-        url = f"{BASE_URL}/api/generate"
-        payload = {
-            "model": model_name, 
-            "prompt": prompt, 
-            "system": system, 
-            "template": template, 
-            "context": context, 
+
+def do(method, url, **kwargs):
+    response = requests.request(method, urljoin(base, url), **kwargs)
+    response.raise_for_status()
+    return response
+
+
+def stream_response(response):
+    for lines in response.iter_lines():
+        for line in lines.splitlines():
+            chunk = json.loads(line)
+            if error := chunk.get('error'):
+                raise Exception(error)
+
+            yield chunk
+
+
+def generate(model_name, prompt, system='', template='', context=[], options={}, stream=False):
+    '''
+    Generate a response for a given prompt with a provided model. This is a streaming endpoint, so
+    will be a series of responses. The final response object will include statistics and additional
+    data from the request.
+    '''
+    response = do('POST', '/api/generate', stream=True, json={
+        k: v for k, v in {
+            "model": model_name,
+            "prompt": prompt,
+            "system": system,
+            "template": template,
+            "context": context,
             "options": options
-        }
-        
-        # Remove keys with None values
-        payload = {k: v for k, v in payload.items() if v is not None}
-        
-        with requests.post(url, json=payload, stream=True) as response:
-            response.raise_for_status()
-            
-            # Creating a variable to hold the context history of the final chunk
-            final_context = None
-            
-            # Variable to hold concatenated response strings if no callback is provided
-            full_response = ""
+        }.items() if v
+    })
 
-            # Iterating over the response line by line and displaying the details
-            for line in response.iter_lines():
-                if line:
-                    # Parsing each line (JSON chunk) and extracting the details
-                    chunk = json.loads(line)
-                    
-                    # If a callback function is provided, call it with the chunk
-                    if callback:
-                        callback(chunk)
-                    else:
-                        # If this is not the last chunk, add the "response" field value to full_response and print it
-                        if not chunk.get("done"):
-                            response_piece = chunk.get("response", "")
-                            full_response += response_piece
-                            print(response_piece, end="", flush=True)
-                    
-                    # Check if it's the last chunk (done is true)
-                    if chunk.get("done"):
-                        final_context = chunk.get("context")
-            
-            # Return the full response and the final context
-            return full_response, final_context
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
-        return None, None
+    if stream:
+        return stream_response(response)
 
-# Create a model from a Modelfile. Use the callback function to override the default handler.
-def create(model_name, model_path, callback=None):
-    try:
-        url = f"{BASE_URL}/api/create"
-        payload = {"name": model_name, "path": model_path}
-        
-        # Making a POST request with the stream parameter set to True to handle streaming responses
-        with requests.post(url, json=payload, stream=True) as response:
-            response.raise_for_status()
+    text = ''
+    for chunk in stream_response(response):
+        if r := chunk.get('response', ''):
+            text += r
+        if chunk.get('done'):
+            chunk['response'] = text
+            chunk['status'] = 'success'
+            return chunk
 
-            # Iterating over the response line by line and displaying the status
-            for line in response.iter_lines():
-                if line:
-                    # Parsing each line (JSON chunk) and extracting the status
-                    chunk = json.loads(line)
 
-                    if callback:
-                        callback(chunk)
-                    else:
-                        print(f"Status: {chunk.get('status')}")
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
+def create(model_name, model_path, stream=False):
+    '''
+    Create a model from a Modelfile.
+    '''
+    response = do('POST', '/api/create', json={'name': model_name, 'path': model_path})
 
-# Pull a model from a the model registry. Cancelled pulls are resumed from where they left off, and multiple
-# calls to will share the same download progress. Use the callback function to override the default handler.
-def pull(model_name, insecure=False, callback=None):
-    try:
-        url = f"{BASE_URL}/api/pull"
-        payload = {
-            "name": model_name,
-            "insecure": insecure
-        }
+    if stream:
+        return stream_response(response)
 
-        # Making a POST request with the stream parameter set to True to handle streaming responses
-        with requests.post(url, json=payload, stream=True) as response:
-            response.raise_for_status()
+    for chunk in stream_response(response):
+        pass
 
-            # Iterating over the response line by line and displaying the details
-            for line in response.iter_lines():
-                if line:
-                    # Parsing each line (JSON chunk) and extracting the details
-                    chunk = json.loads(line)
+    return chunk
 
-                    # If a callback function is provided, call it with the chunk
-                    if callback:
-                        callback(chunk)
-                    else:
-                        # Print the status message directly to the console
-                        print(chunk.get('status', ''), end='', flush=True)
-                    
-                    # If there's layer data, you might also want to print that (adjust as necessary)
-                    if 'digest' in chunk:
-                        print(f" - Digest: {chunk['digest']}", end='', flush=True)
-                        print(f" - Total: {chunk['total']}", end='', flush=True)
-                        print(f" - Completed: {chunk['completed']}", end='\n', flush=True)
-                    else:
-                        print()
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
 
-# Push a model to the model registry. Use the callback function to override the default handler.
-def push(model_name, insecure=False, callback=None):
-    try:
-        url = f"{BASE_URL}/api/push"
-        payload = {
-            "name": model_name,
-            "insecure": insecure
-        }
+def pull(model_name, insecure=False, stream=False):
+    '''
+    Pull a model from a the model registry. Cancelled pulls are resumed from where they left off,
+    and multiple calls to will share the same download progress.
+    '''
+    response = do('POST', '/api/pull', stream=True, json={'name': model_name, 'insecure': insecure})
 
-        # Making a POST request with the stream parameter set to True to handle streaming responses
-        with requests.post(url, json=payload, stream=True) as response:
-            response.raise_for_status()
+    if stream:
+        return stream_response(response)
 
-            # Iterating over the response line by line and displaying the details
-            for line in response.iter_lines():
-                if line:
-                    # Parsing each line (JSON chunk) and extracting the details
-                    chunk = json.loads(line)
+    layers = {}
+    for chunk in stream_response(response):
+        if digest := chunk.pop('digest', None):
+            layers[digest] = chunk
 
-                    # If a callback function is provided, call it with the chunk
-                    if callback:
-                        callback(chunk)
-                    else:
-                        # Print the status message directly to the console
-                        print(chunk.get('status', ''), end='', flush=True)
-                    
-                    # If there's layer data, you might also want to print that (adjust as necessary)
-                    if 'digest' in chunk:
-                        print(f" - Digest: {chunk['digest']}", end='', flush=True)
-                        print(f" - Total: {chunk['total']}", end='', flush=True)
-                        print(f" - Completed: {chunk['completed']}", end='\n', flush=True)
-                    else:
-                        print()
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
+    layers['status'] = 'success'
+    return layers
 
-# List models that are available locally.
+
+def push(model_name, insecure=False, stream=False):
+    '''
+    Push a model to the model registry.
+    '''
+    response = do('POST', '/api/push', stream=True, json={'name': model_name, 'insecure': insecure})
+
+    if stream:
+        return stream_response(response)
+
+    layers = {}
+    for chunk in stream_response(response):
+        if digest := chunk.pop('digest', None):
+            layers[digest] = chunk
+
+    layers['status'] = 'success'
+    return layers
+
+
 def list():
-    try:
-        response = requests.get(f"{BASE_URL}/api/tags")
-        response.raise_for_status()
-        data = response.json()
-        models = data.get('models', [])
-        return models
+    '''
+    List models that are available locally.
+    '''
+    response = do('GET', '/api/tags')
+    return response.json().get('models') or []
 
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
-        return None
 
-# Copy a model. Creates a model with another name from an existing model.
 def copy(source, destination):
-    try:
-        # Create the JSON payload
-        payload = {
-            "source": source,
-            "destination": destination
-        }
-        
-        response = requests.post(f"{BASE_URL}/api/copy", json=payload)
-        response.raise_for_status()
-        
-        # If the request was successful, return a message indicating that the copy was successful
-        return "Copy successful"
+    '''
+    Copy a model. Creates a model with another name from an existing model.
+    '''
+    response = do('POST', '/api/copy', json={'source': source, 'destination': destination})
+    return {'status': 'success' if response.status_code == 200 else 'error'}
 
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
-        return None
 
-# Delete a model and its data.
 def delete(model_name):
-    try:
-        url = f"{BASE_URL}/api/delete"
-        payload = {"name": model_name}
-        response = requests.delete(url, json=payload)
-        response.raise_for_status()
-        return "Delete successful"
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
-        return None
+    '''
+    Delete a model and its data.
+    '''
+    response = do('DELETE', '/api/delete', json={'name': model_name})
+    return {'status': 'success' if response.status_code == 200 else 'error'}
 
-# Show info about a model.
+
 def show(model_name):
-    try:
-        url = f"{BASE_URL}/api/show"
-        payload = {"name": model_name}
-        response = requests.post(url, json=payload)
-        response.raise_for_status()
-        
-        # Parse the JSON response and return it
-        data = response.json()
-        return data
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
-        return None
-
-def heartbeat():
-    try:
-        url = f"{BASE_URL}/"
-        response = requests.head(url)
-        response.raise_for_status()
-        return "Ollama is running"
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
-        return "Ollama is not running"
+    '''
+    Show info about a model.
+    '''
+    return do('POST', '/api/show', json={'name': model_name}).json()
 
 
+def ping():
+    '''
+    Ping the server to check if it is running.
+    '''
+    response = do('HEAD', '/')
+    return {'status': 'success' if response.status_code == 200 else 'error'}
